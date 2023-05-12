2e9ee6ec4d40bfc20a1d5b9060e306baece85890
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-12 10:55:36 -0300

    implement get all players controller

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import CreatePlayerDto from './dtos/create_player.dto';
 import { PlayersService } from './players.service';
+import { IPlayer } from './interfaces/players.interface';
 
 @Controller('api/v1/players')
 export class PlayersController {
@@ -9,5 +10,10 @@ export class PlayersController {
   @Post()
   async createUpdatePlayer(@Body() createPlayerDto: CreatePlayerDto) {
     await this.playerService.createUpdatePlayer(createPlayerDto);
+  }
+
+  @Get()
+  async getPlayers(): Promise<IPlayer[]> {
+    return this.playerService.getAll();
   }
 }
