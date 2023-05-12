57ecbf920681d5f17593ba3e2d7faf04cae46888
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-12 10:36:31 -0300

    define create private method

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import CreatePlayerDto from './dtos/create_player.dto';
+import { IPlayer } from './interfaces/players.interface';
+import { v4 as uuid4 } from 'uuid';
 
 @Injectable()
-export class PlayersService {}
+export class PlayersService {
+  private readonly logger = new Logger(PlayersService.name);
+  private players: IPlayer[] = [];
+
+  async createUpdatePlayer(createPlayerDto: CreatePlayerDto): Promise<void> {
+    this.logger.log(`createPplayerDto ${createPlayerDto}`);
+    this.create(createPlayerDto);
+  }
+
+  private create(createPlayerDto: CreatePlayerDto): void {
+    const { email, name, phoneNumber } = createPlayerDto;
+
+    const player: IPlayer = {
+      _id: uuid4(),
+      name,
+      email,
+      phoneNumber,
+      ranking: 'A',
+      rankingPosition: 1,
+      urlImagePalyer: '',
+    };
+    this.players.push(player);
+  }
+}
