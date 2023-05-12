a5e1a412a3d6a71724881423b5865b46c30b15de
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-12 14:43:48 -0300

    add dotenv dependence

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PlayersModule } from './players/players.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
+
+config();
 
 const MONGODB = process.env.MONGO_DB;
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
+config();
 
 const PORT = process.env.PORT;
 
