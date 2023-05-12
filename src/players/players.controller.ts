960c7d1d293bb72cc82bca961db157da100ce39d
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-12 10:13:08 -0300

    apply dto in controller in create player method

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import CreatePlayerDto from './dtos/create_player.dto';
 
 @Controller('api/v1/players')
 export class PlayersController {
   @Post()
-  async createUpdatePlayer() {
-    return JSON.stringify({
-      name: 'anderson',
-    });
+  async createUpdatePlayer(@Body() createPlayerDto: CreatePlayerDto) {
+    const { email } = createPlayerDto;
+
+    return JSON.stringify(`{
+      email: ${email},
+    }`);
   }
 }
