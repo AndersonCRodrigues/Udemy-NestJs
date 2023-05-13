4fb78bf488b8bc108364eeb5f4de437c4222815a
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 10:33:25 -0300

    feat: add imports model category

--- src/categoies/categoies.module.ts
+++ src/categoies/categoies.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CategoiesController } from './categoies.controller';
 import { CategoiesService } from './categoies.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategorySchema } from './interfaces/category.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema }]),
+  ],
   controllers: [CategoiesController],
-  providers: [CategoiesService]
+  providers: [CategoiesService],
 })
 export class CategoiesModule {}
