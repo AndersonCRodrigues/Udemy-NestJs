f3eb734cdbc0ecd312289791fd4c2aa0a69888c0
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 10:32:58 -0300

    feat: add create category service

--- src/categoies/categoies.service.ts
+++ src/categoies/categoies.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ICategory } from './interfaces/category.interface';
+import { CreateCategoryDto } from './dtos/create_category.dto';
 
 @Injectable()
-export class CategoiesService {}
+export class CategoiesService {
+  constructor(
+    @InjectModel('Category') private readonly categoryModel: Model<ICategory>,
+  ) {}
+
+  private async findCategory(category: string) {
+    return this.categoryModel.findOne({ category });
+  }
+
+  async createCategory(
+    createCategoryDto: CreateCategoryDto,
+  ): Promise<ICategory> {
+    const { category: param } = createCategoryDto;
+    if (await this.findCategory(param)) {
+      throw new BadRequestException('Category already registered');
+    }
+    const category = new this.categoryModel(createCategoryDto);
+    return category.save();
+  }
+}
