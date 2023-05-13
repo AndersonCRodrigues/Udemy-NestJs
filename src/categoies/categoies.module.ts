98c46e64888e3cffbdfa347338084433e733e534
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 11:49:43 -0300

    refactory: imports players schema

--- src/categoies/categoies.module.ts
+++ src/categoies/categoies.module.ts
@@ -3,12 +3,23 @@ import { CategoiesController } from './categoies.controller';
 import { CategoiesService } from './categoies.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategorySchema } from './interfaces/category.schema';
+import { PlayersService } from 'src/players/players.service';
+import { PlayerSchema } from 'src/players/interfaces/player.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema }]),
+    MongooseModule.forFeature([
+      {
+        name: 'Category',
+        schema: CategorySchema,
+      },
+      {
+        name: 'Player',
+        schema: PlayerSchema,
+      },
+    ]),
   ],
   controllers: [CategoiesController],
-  providers: [CategoiesService],
+  providers: [CategoiesService, PlayersService],
 })
 export class CategoiesModule {}
