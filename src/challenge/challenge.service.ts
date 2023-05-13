ca0f757d23270a387c63e751f519896246d1c8e0
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 15:27:25 -0300

    feat: add create challenge service

--- src/challenge/challenge.service.ts
+++ src/challenge/challenge.service.ts
@@ -1,22 +1,48 @@
-import { Injectable } from '@nestjs/common';
-import { CategoiesService } from 'src/categories/categories.service';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CategoriesService } from 'src/categories/categories.service';
 import { PlayersService } from 'src/players/players.service';
 import { CreateChallengeDto } from './dtos/create_challenge.dto';
+import { IChallenge } from './interfaces/challenge.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ChallengeService {
   constructor(
-    private readonly categoryService: CategoiesService,
+    @InjectModel('Challenge')
+    private readonly challengeModel: Model<IChallenge>,
+    private readonly categoryService: CategoriesService,
     private readonly playerService: PlayersService,
   ) {}
 
-  async createChallenge(
+  private async checkPlayers(
     createChallengeDto: CreateChallengeDto,
-  ): Promise<IChallenge> {
+  ): Promise<void> {
     createChallengeDto.players.forEach(
       async (player) => await this.playerService.getById(player._id),
     );
 
-    const players = await this.playerService.getAll();
+    if (
+      !createChallengeDto.players.some(
+        (player) => player._id === createChallengeDto.requester,
+      )
+    )
+      throw new BadRequestException(
+        'The requesting Player must be in the match',
+      );
+  }
+
+  async createChallenge(
+    createChallengeDto: CreateChallengeDto,
+  ): Promise<IChallenge> {
+    await this.checkPlayers(createChallengeDto);
+    const category = await this.categoryService.getCategoryByPlayer(
+      createChallengeDto.requester,
+    );
+
+    const challenge = new this.challengeModel(createChallengeDto);
+    challenge.category = category.category;
+
+    return challenge.save();
   }
 }
