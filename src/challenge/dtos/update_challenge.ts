1bb4771a58012c05b2949fc1eac3fd4fcb720046
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 16:51:23 -0300

    feat: add Update challenge dto

---
+++ src/challenge/dtos/update_challenge.ts
@@ -0,0 +1,11 @@
+import { IsDateString, IsNotEmpty, IsOptional } from 'class-validator';
+import { ChallengeStatus } from '../interfaces/challenge.status.enum';
+
+export class UpdateChallengeDto {
+  @IsNotEmpty()
+  @IsDateString()
+  dateHourChallenge: Date;
+
+  @IsOptional()
+  sataus: ChallengeStatus;
+}
