560341ee6517991eacf94dc95bb6d15e1ed12141
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 09:30:13 -0300

    feat: add ICategory interface

---
+++ src/categoies/interfaces/category.interface.ts
@@ -0,0 +1,15 @@
+import { Document } from 'mongoose';
+import { IPlayer } from 'src/players/interfaces/players.interface';
+
+export type Event = {
+  name: string;
+  operation: string;
+  value: number;
+};
+
+export interface ICategory extends Document {
+  readonly category: string;
+  decription: string;
+  events: Event[];
+  players: IPlayer[];
+}
