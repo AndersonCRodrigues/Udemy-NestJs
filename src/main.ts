88672242b8cac443485a223823f4cb3472457783
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 15:44:49 -0300

    add moment timezone dependence

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { AllExceptionsFilter } from './common/filters/http.exception.filter';
+import * as momentTimezone from 'moment-timezone';
 
 config();
 
@@ -11,6 +12,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  Date.prototype.toJSON = (): any => {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss:SSS');
+  };
 
   await app.listen(PORT || 8080);
 }
