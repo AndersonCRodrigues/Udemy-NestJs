c6cf9c668e80148ae54508e916a7ad730996e6f1
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 09:23:19 -0300

    feat: add vategories module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { PlayersModule } from './players/players.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { config } from 'dotenv';
+import { CategoiesModule } from './categoies/categoies.module';
 
 config();
 
 const MONGODB = process.env.MONGO_DB;
 
 @Module({
-  imports: [MongooseModule.forRoot(MONGODB), PlayersModule],
+  imports: [MongooseModule.forRoot(MONGODB), PlayersModule, CategoiesModule],
   controllers: [],
   providers: [],
 })
