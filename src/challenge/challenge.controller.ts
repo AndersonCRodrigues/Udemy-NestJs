dabbc0e75cd857781a3272f998aeb78f04e7f6bc
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 14:43:11 -0300

    fix imports

--- src/challenge/challenge.controller.ts
+++ src/challenge/challenge.controller.ts
@@ -1,16 +1,23 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ChallengeService } from './challenge.service';
 import { CreateChallengeDto } from './dtos/create_challenge.dto';
+import { IChallenge } from './interfaces/challenge.interface';
 
 @Controller('/api/v1/challenge')
 export class ChallengeController {
-
-  constructor(provate readonly challengeService: ChallengeService) {}
+  constructor(private readonly challengeService: ChallengeService) {}
 
   @Post()
   @UsePipes(ValidationPipe)
   async createChallenge(
-    @Body() createChallengeDto: CreateChallengeDto): Promise<IChallenge> {
-      return this.challengeService.createChallenge(createChallengeDto)
-    }
+    @Body() createChallengeDto: CreateChallengeDto,
+  ): Promise<IChallenge> {
+    return this.challengeService.createChallenge(createChallengeDto);
+  }
 }
