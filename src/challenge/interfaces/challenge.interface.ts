0ecba01327af3174c5adfb28fe0c2b48336da741
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 14:35:49 -0300

    feat: add challenge interface

---
+++ src/challenge/interfaces/challenge.interface.ts
@@ -0,0 +1,24 @@
+import { Document } from 'mongoose';
+import { ChallengeStatus } from './challenge.status.enum';
+import { IPlayer } from 'src/players/interfaces/players.interface';
+
+export interface IResult {
+  set: string;
+}
+
+export interface IMatch extends Document {
+  category: string;
+  players: IPlayer[];
+  def: IPlayer;
+  result: IResult[];
+}
+
+export interface IChallenge extends Document {
+  dateHourChallenge: Date;
+  status: ChallengeStatus;
+  dateHourRequest: Date;
+  dateHourResponse: Date;
+  requester: string;
+  players: IPlayer[];
+  match: IMatch;
+}
