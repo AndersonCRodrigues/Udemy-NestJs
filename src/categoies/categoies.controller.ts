caad13bae20fd9c0d2054def2003b99055d0d2aa
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 10:33:52 -0300

    feat: add create category controller

--- src/categoies/categoies.controller.ts
+++ src/categoies/categoies.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateCategoryDto } from './dtos/create_category.dto';
+import { ICategory } from './interfaces/category.interface';
+import { CategoiesService } from './categoies.service';
 
-@Controller('categoies')
-export class CategoiesController {}
+@Controller('api/v1/categoies')
+export class CategoiesController {
+  constructor(private readonly categoriesService: CategoiesService) {}
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async createCategory(
+    @Body() createCategoryDto: CreateCategoryDto,
+  ): Promise<ICategory> {
+    return this.categoriesService.createCategory(createCategoryDto);
+  }
+}
