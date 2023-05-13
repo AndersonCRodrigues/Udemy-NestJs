059fdc989b88ad4d35b3f50c47b4b6e62bdf22d9
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 14:25:59 -0300

    feat: add challenge schema

---
+++ src/challenge/interfaces/challenge.schema.ts
@@ -0,0 +1,27 @@
+import * as mongoose from 'mongoose';
+
+export const ChallengeSchema = new mongoose.Schema(
+  {
+    dateHourChallenge: { type: Date },
+    status: { type: String },
+    dateHourRequest: { type: Date },
+    dateHourResponse: { type: Date },
+    requester: { type: String },
+    players: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Player',
+      },
+    ],
+    match: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Match',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    collection: 'challenges',
+  },
+);
