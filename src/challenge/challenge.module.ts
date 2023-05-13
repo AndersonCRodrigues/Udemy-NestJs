9cd28c6a957388bcfef129f4c4f5b3a113c381a9
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 14:51:36 -0300

    imports challenge schema and match schema

--- src/challenge/challenge.module.ts
+++ src/challenge/challenge.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { ChallengeController } from './challenge.controller';
 import { ChallengeService } from './challenge.service';
 import { CategoriesModule } from 'src/categories/categories.module';
+import { PlayersModule } from 'src/players/players.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ChallengeSchema } from './interfaces/challenge.schema';
+import { MatchSchema } from './interfaces/match.schema';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [
+    CategoriesModule,
+    PlayersModule,
+    MongooseModule.forFeature([
+      { name: 'Challenge', schema: ChallengeSchema },
+      { name: 'Math', schema: MatchSchema },
+    ]),
+  ],
   controllers: [ChallengeController],
   providers: [ChallengeService],
 })
