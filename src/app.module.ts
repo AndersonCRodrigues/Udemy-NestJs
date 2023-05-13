2d43955fb038bde47404a57f2d2e12093aced4aa
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-13 13:39:30 -0300

    feat: add entity challenge

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { PlayersModule } from './players/players.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { config } from 'dotenv';
 import { CategoiesModule } from './categoies/categoies.module';
+import { ChallengeModule } from './challenge/challenge.module';
 
 config();
 
 const MONGODB = process.env.MONGO_DB;
 
 @Module({
-  imports: [MongooseModule.forRoot(MONGODB), PlayersModule, CategoiesModule],
+  imports: [MongooseModule.forRoot(MONGODB), PlayersModule, CategoiesModule, ChallengeModule],
   controllers: [],
   providers: [],
 })
