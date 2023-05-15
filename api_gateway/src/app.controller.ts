134d3f42ce2a2a275bd43cf32d516ff830f08d1b
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-15 17:45:30 -0300

    add RQM connection

--- api_gateway/src/app.controller.ts
+++ api_gateway/src/app.controller.ts
@@ -1,16 +1,23 @@
-import { Controller, Logger } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Logger,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import {
   ClientProxy,
   ClientProxyFactory,
   Transport,
 } from '@nestjs/microservices';
 import { config } from 'dotenv';
+import { CreateCategoryDto } from './dtos/create_category.dto';
+config();
 
 const END_POINT = process.env.RABBIT_URL || 'no_url';
 
-config();
-
-@Controller('api/v1')
+@Controller('api/v1/categories')
 export class AppController {
   private logger = new Logger(AppController.name);
   private clienteAdminBackend: ClientProxy;
@@ -21,7 +28,14 @@ export class AppController {
       options: {
         urls: [END_POINT],
         queue: 'admin-backend',
+        noAck: false,
       },
     });
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async createCategory(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.clienteAdminBackend.emit('create-category', createCategoryDto);
   }
 }
