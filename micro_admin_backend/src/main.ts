ac9fbbd14b645c9d9746909226fecd207bd031ad
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-15 19:58:00 -0300

    feat: microservice connection

--- micro_admin_backend/src/main.ts
+++ micro_admin_backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+config();
+
+const URL = process.env.RABBIT_URL || 'no_url';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [URL],
+      queue: 'admin-backend',
+    },
+  });
+
+  (await app).listen();
 }
 bootstrap();
