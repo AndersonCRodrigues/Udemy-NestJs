472b00b2e07cf58457c6b1025f48a4c3debc1319
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-16 14:44:00 -0300

    feat: implements timeout interceptor

--- api_gateway/src/main.ts
+++ api_gateway/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { AllExceptionsFilter } from './filters/http.exception.filter';
+import { LogginInterceptor } from './interceptors/logging.interceptor';
+import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 
 config();
 
@@ -10,6 +12,7 @@ const { PORT } = process.env;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalInterceptors(new LogginInterceptor(), new TimeoutInterceptor());
   app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(PORT || 8080);
