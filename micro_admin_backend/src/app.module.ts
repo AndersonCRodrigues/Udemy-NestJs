b22f8849556736ca6f03141b5cdc3f1e11634276
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-16 13:06:23 -0300

    feat: add mongoDB config

--- micro_admin_backend/src/app.module.ts
+++ micro_admin_backend/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
+import { CategorySchema } from './interfaces/categories/category.schema';
+import { PlayerSchema } from './interfaces/players/player.schema';
+config();
+
+const { MONGODB } = process.env;
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(MONGODB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true,
+    }),
+    MongooseModule.forFeature([
+      { name: 'Category', schema: CategorySchema },
+      { name: 'Player', schema: PlayerSchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
