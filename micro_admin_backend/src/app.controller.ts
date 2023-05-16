db43787500375d97085da3f97a845a0237abba3d
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-05-16 15:03:14 -0300

    feat: implements ctx rmq

--- micro_admin_backend/src/app.controller.ts
+++ micro_admin_backend/src/app.controller.ts
@@ -1,7 +1,15 @@
 import { Controller, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
-import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
+import {
+  Ctx,
+  EventPattern,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 import { ICategory } from './interfaces/categories/category.interface';
+
+const ackErrors = ['E1100'];
 
 @Controller()
 export class AppController {
@@ -10,9 +18,24 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @EventPattern('create-category')
-  async createCategory(@Payload() category: ICategory) {
-    // this.logger.log(`category: ${JSON.stringify(category)}`);
-    return await this.appService.createCategory(category);
+  async createCategory(
+    @Payload() category: ICategory,
+    @Ctx() context: RmqContext,
+  ) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    try {
+      this.logger.log(`category: ${JSON.stringify(category)}`);
+      await this.appService.createCategory(category);
+      await channel.ack(originalMsg);
+    } catch (e) {
+      this.logger.log(`error: ${JSON.stringify(e.message)}`);
+      ackErrors.forEach(async (ackError) => {
+        if (e.message.includes(ackError)) {
+          await channel.ack(originalMsg);
+        }
+      });
+    }
   }
 
   @MessagePattern('get-categories')
